e7fa5926ea6e1cbbb226721774916c3014830a36
Pavithra Thathsarani <pavithrathathsarani26@gmail.com>
2025-01-06 15:09:43 +0100

    updates to submitQuiz

--- my-simple-react-app/src/graphql/mutations/quiz.ts
+++ my-simple-react-app/src/graphql/mutations/quiz.ts
@@ -1,11 +1,17 @@
 import { gql } from '@apollo/client';
 
 export const SUBMIT_QUIZ = gql`
-  mutation SubmitQuiz($userId: ID!, $quizId: ID!, $answers: [Int!]!) {
-    submitQuiz(userId: $userId, quizId: $quizId, answers: $answers) {
+  mutation SubmitQuiz($userId: ID!, $quizId: ID!, $answers: [Int!]!, $isRecommended: Boolean) {
+    submitQuiz(userId: $userId, quizId: $quizId, answers: $answers, isRecommended: $isRecommended) {
       id
       success
       message
+      cluster
+      questions {
+        prompt
+        options
+        correctAnswer
+      }
     }
   }
 `;
